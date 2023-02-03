6792839e4e488bf6bfe6dcf4bdbde4b17f34b235
Glaicon <gjpeixer@gmail.com>
2023-02-03 14:18:48 -0800

    update marketplace index

--- src/views/marketplace/index.tsx
+++ src/views/marketplace/index.tsx
@@ -2,35 +2,19 @@
 import { FC, useEffect, useState } from "react";
 // Wallet
 import { useWallet, useConnection } from "@solana/wallet-adapter-react";
-
 // Components
 import pkg from "../../../package.json";
-import { AuctionHouseProgram } from "@metaplex-foundation/mpl-auction-house";
 
 // Store
 import useUserSOLBalanceStore from "../../stores/useUserSOLBalanceStore";
-import { CounterView } from "views/counter";
-import { GalleryView } from "views/gallery";
-import { MintView } from "views/mint";
 import {
-  AuctionHouse,
-  CreateAuctionHouseOutput,
   Metadata,
   Metaplex,
   Nft,
   PublicKey,
-  SolAmount,
   walletAdapterIdentity,
 } from "@metaplex-foundation/js";
-import {
-  Keypair,
-  SYSVAR_INSTRUCTIONS_PUBKEY,
-  Transaction,
-} from "@solana/web3.js";
-import {
-  createPrintListingReceiptInstruction,
-  createSellInstruction,
-} from "@metaplex-foundation/mpl-auction-house/dist/src/generated/instructions";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 export const MarketplaceView: FC = ({}) => {
   const wallet = useWallet();
@@ -40,6 +24,7 @@ export const MarketplaceView: FC = ({}) => {
   const [auctionHouse, setAuctionHouse] = useState(null);
   const [nft, setNft] = useState(null);
   const [status, setStatus] = useState("");
+  const [listings, setListings] = useState([]);
 
   const WRAPPED_SOL_MINT = new PublicKey(
     "So11111111111111111111111111111111111111112"
@@ -88,177 +73,48 @@ export const MarketplaceView: FC = ({}) => {
     }
   };
 
-  // const createListing = async (nftParam: Nft) => {
-  //   mx.use(walletAdapterIdentity(wallet));
-  //   try {
-  //     // mx.auctionHouse().builders().list({
-
-  //     // })
-
-  //     let auctionHouseByCreator = await mx.auctionHouse().findByCreatorAndMint({
-  //       creator: wallet.publicKey,
-  //       treasuryMint: WRAPPED_SOL_MINT,
-  //     });
-  //     debugger;
-  //     const listtx = await mx
-  //       .auctionHouse()
-  //       .builders()
-  //       .list({
-  //         auctionHouse: auctionHouseByCreator, // A model of the Auction House related to this listing
-  //         seller: Keypair.generate(), // Creator of a listing
-  //         authority: Keypair.generate(),
-  //         // authority: wallet.publicKey, // The Auction House authority
-  //         mintAccount: nftParam.address, // The mint account to create a listing for, used to find the metadata
-  //         tokenAccount: nftParam.address, // The token account address that's associated to the asset a listing created is for,
-  //         price: 1.2000 as any,
-  //       });
-  //     debugger;
-  //     var rpcClient = await mx.rpc().sendTransaction(listtx);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // const createListing = async (nftParam: Nft) => {
-  //   mx.use(walletAdapterIdentity(wallet));
-  //   try {
-  //     // mx.auctionHouse().builders().list({
-
-  //     // })
-
-  //     let auctionHouseByCreator = await mx.auctionHouse().findByCreatorAndMint({
-  //       creator: wallet.publicKey,
-  //       treasuryMint: WRAPPED_SOL_MINT,
-  //     });
-  //     debugger;
-  //     const listtx = await mx
-  //       .auctionHouse()
-  //       .builders()
-  //       .list({
-  //         auctionHouse: auctionHouseByCreator, // A model of the Auction House related to this listing
-  //         seller: Keypair.generate(), // Creator of a listing
-  //         authority: Keypair.generate(),
-  //         // authority: wallet.publicKey, // The Auction House authority
-  //         mintAccount: nftParam.address, // The mint account to create a listing for, used to find the metadata
-  //         tokenAccount: nftParam.address, // The token account address that's associated to the asset a listing created is for,
-  //         price: 1.2000 as any,
-  //       });
-  //     debugger;
-  //     var rpcClient = await mx.rpc().sendTransaction(listtx);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  const createListing = async (amount, nft: Nft) => {
+  const createListing = async (amount, nftParam: Nft) => {
     mx.use(walletAdapterIdentity(wallet));
-    const { publicKey } = wallet;
-    let auctionHouse2 = await mx.auctionHouse().findByCreatorAndMint({
-      creator: wallet.publicKey,
-      treasuryMint: WRAPPED_SOL_MINT,
-    });
-    // const auctionHouse2: CreateAuctionHouseOutput = await mx.auctionHouse().create({
-    //   sellerFeeBasisPoints: 500,
-    //   authority: mx.identity().publicKey,
-    //   requiresSignOff: false,
-    //   canChangeSalePrice: true,
-    //   treasuryMint: WRAPPED_SOL_MINT
-    // });
-    debugger;
-    const ah: AuctionHouse = auctionHouse2;
-    const buyerPrice = amount;
-    const auctionHouse = new PublicKey(ah.address);
-    const authority = new PublicKey(ah.authorityAddress);
-    const auctionHouseFeeAccount = new PublicKey(ah.feeAccountAddress);
-    const treasuryMint = new PublicKey(ah.treasuryMint.address);
-    const tokenMint = new PublicKey(nft.mint.address);
-    const metadata = new PublicKey(nft.metadataAddress);
+    try {
+      let auctionHouseByCreator = await mx.auctionHouse().findByCreatorAndMint({
+        creator: wallet.publicKey,
+        treasuryMint: WRAPPED_SOL_MINT,
+      });
+      
+      await mx.auctionHouse().list({
+        auctionHouse: auctionHouseByCreator,
+        seller: mx.identity(),
+        mintAccount: nftParam.mint.address,
+        price: {
+          basisPoints: amount,
+          currency: { decimals: 9, symbol: "SOL" },
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const associatedTokenAccount = new PublicKey(
-      // nft.edition.address
-      nft.tokenStandard
-      // nft.owner.associatedTokenAccountAddress
-    );
+  const fetchListing = async () => {
+    let auctionHouseByCreator: any = await mx
+      .auctionHouse()
+      .findByCreatorAndMint({
+        creator: wallet.publicKey,
+        treasuryMint: WRAPPED_SOL_MINT,
+      });
 
-    const [sellerTradeState, tradeStateBump] =
-      await AuctionHouseProgram.findTradeStateAddress(
-        publicKey,
-        auctionHouse,
-        associatedTokenAccount,
-        treasuryMint,
-        tokenMint,
-        buyerPrice,
-        1
-      );
+    const listings: any = await mx
+      .auctionHouse()
+      .findListings({ auctionHouse: auctionHouseByCreator });
 
-    const [programAsSigner, programAsSignerBump] =
-      await AuctionHouseProgram.findAuctionHouseProgramAsSignerAddress();
-
-    const [freeTradeState, freeTradeBump] =
-      await AuctionHouseProgram.findTradeStateAddress(
-        publicKey,
-        auctionHouse,
-        associatedTokenAccount,
-        treasuryMint,
-        tokenMint,
-        0,
-        1
-      );
-
-    const txt = new Transaction();
-
-    const sellInstructionArgs = {
-      tradeStateBump,
-      freeTradeStateBump: freeTradeBump,
-      programAsSignerBump: programAsSignerBump,
-      buyerPrice,
-      tokenSize: 1,
-    };
-
-    const sellInstructionAccounts = {
-      wallet: publicKey,
-      tokenAccount: associatedTokenAccount,
-      metadata: metadata,
-      authority: authority,
-      auctionHouse: auctionHouse,
-      auctionHouseFeeAccount: auctionHouseFeeAccount,
-      sellerTradeState: sellerTradeState,
-      freeSellerTradeState: freeTradeState,
-      programAsSigner: programAsSigner,
-    };
-
-    const sellInstruction = createSellInstruction(
-      sellInstructionAccounts,
-      sellInstructionArgs
-    );
-
-    const [receipt, receiptBump] =
-      await AuctionHouseProgram.findListingReceiptAddress(sellerTradeState);
-
-    const printListingReceiptInstruction = createPrintListingReceiptInstruction(
-      {
-        receipt,
-        bookkeeper: publicKey,
-        instruction: SYSVAR_INSTRUCTIONS_PUBKEY,
-      },
-      {
-        receiptBump,
-      }
-    );
-
-    txt.add(sellInstruction).add(printListingReceiptInstruction);
-
-    let latestBlockhash = await connection.getLatestBlockhash();
-
-    // Send transaction and await for signature
-    const signature = await wallet.sendTransaction(txt, connection);
-
-    // Send transaction and await for signature
-    await connection.confirmTransaction(
-      { signature, ...latestBlockhash },
-      "confirmed"
-    );
-    // var rpcClient = await mx.rpc().sendTransaction(txt);
+    for (let index = 0; index < listings.length; index++) {
+      const element = listings[index].metadataAddress;
+      listings[index].nft = await mx
+        .nfts()
+        .findByMetadata({ metadata: element });
+    }
+    
+    setListings(listings);
   };
 
   return (
@@ -282,6 +138,12 @@ export const MarketplaceView: FC = ({}) => {
           </button>
           <button
             className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black  grid w-full"
+            onClick={fetchListing}
+          >
+            <span>SHOW MY LISTINGS </span>
+          </button>
+          <button
+            className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black  grid w-full"
             onClick={fetchNft}
           >
             <span>SHOW MY NFTS </span>
@@ -301,6 +163,23 @@ export const MarketplaceView: FC = ({}) => {
                     <h3>{x.symbol}</h3>
                     <img
                       src={x.json.image}
+                      alt="The downloaded illustration of the provided NFT address."
+                    />
+                  </div>
+                </>
+              ))}
+          </div>
+          <div className="hero-content grid-cols-3">
+            {listings &&
+              listings.map((x) => (
+                <>
+                  <div key={x.nft.uri} className="h-60 w-60 flex-row flex-wrap">
+                    <h1>
+                      {x.nft.name} - SOL {x.price?.basisPoints.toNumber()}
+                    </h1>
+                    <h3>{x.nft.symbol}</h3>
+                    <img
+                      src={x.nft.json.image}
                       alt="The downloaded illustration of the provided NFT address."
                     />
                   </div>
