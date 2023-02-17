877e1f1cc0ad4f55f5a89afd4829638028808ad5
Glaicon <gjpeixer@gmail.com>
2023-02-17 09:48:14 -0800

    Remove all pack nft references

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -2,4 +2,3 @@ export { HomeView } from "./home";
 export { BasicsView } from "./basics";
 export { MarketplaceView } from "./marketplace";
 export { CandyView } from "./candy";
-export { PackView } from "./pack";
